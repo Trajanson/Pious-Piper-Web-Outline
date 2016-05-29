7372821d2c871c6ec0f5f0655ec889dc1c294e80
Julian Trajanson <julian.trajanson@gmail.com>
2016-05-28 20:25:32 -0400

    add basic scaffold

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('piper', {title: 'Pious Piper'})
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
