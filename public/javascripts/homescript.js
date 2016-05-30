d01d83c5c63a52c5a9c675bdd07efc13d7a3409e
Julian Trajanson <julian.trajanson@gmail.com>
2016-05-30 19:18:29 -0400

    early file system with likely bugs

--- public/javascripts/homescript.js
+++ public/javascripts/homescript.js
@@ -1,10 +1,14 @@
+// SETTINGS ///////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+const TEXT_FILE_UPLOAD_URL_FOR_PIOUS = "/pious/uploadText"
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
 var valid_form = true;
 
 $(window).ready(function() {
 
   $('#funky').attr('disabled', true);
-
-  $('#getPiousForm').hide();
 
   $('.container').on('click', function() {
     getPiousButtonClickEvent();
@@ -22,7 +26,7 @@ var attachPiousFileAutoLoader = function() {
 
     switch(val.substring(val.lastIndexOf('.') + 1).toLowerCase()){
         case 'txt':
-            engageDownloadAnimation();
+            submitTextFileAJAX();
             break;
         default:
             $(this).val('');
@@ -37,10 +41,12 @@ var attachPiousFileAutoLoader = function() {
   };
 }
 
-var fileHasSuccessfullyUploaded = function() {
-  document.getElementById("piousFile").submit();
-  engageDownloadAnimation();
-}
+
+
+// var fileHasSuccessfullyUploaded = function() {
+//   document.getElementById("piousFile").submit();
+//   engageDownloadAnimation();
+// }
 
 var innappropriateFileHasBeenUploaded = function() {
   location.reload();
@@ -63,4 +69,38 @@ var getPiousButtonClickEvent = function() {
 
 var engageDownloadAnimation = function() {
   $('#funky').attr('checked', 'checked');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+var submitTextFileAJAX = function() {
+  var fd = new FormData();
+  fd.append( 'piousFile', $('#piousFile')[0].files[0]);
+
+  $.ajax({
+    url: TEXT_FILE_UPLOAD_URL_FOR_PIOUS,
+    data: fd,
+    processData: false,
+    contentType: false,
+    type: 'POST',
+    success: function(data){
+      // window.open('/pious/getPiousSample', '_blank');
+      engageDownloadAnimation();
+      console.log(data);
+      console.log('upload success!')
+      $('#piousFile').empty();
+      // $('#data').append(data);
+
+    }
+  });
 }
--- routes/piousfiles.js
+++ routes/piousfiles.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-
+var multer = require('multer');
+var upload = multer({ dest: 'public/uploads/textfiles'});
 
 
 router.get('/getPiousSample', function(req, res) {
@@ -21,5 +22,47 @@ router.get('/getPiousSample', function(req, res) {
   res.sendFile( filename ,options);
 });
 
+router.post('/uploadText',[ upload.any(), function(req, res, next) {
+  var fileInfo = req.files[0],
+      fileName = fileInfo['filename'];
+  console.log(req.files);
+  console.log(fileInfo);
+
+  var root = __dirname + '/../piousFiles';
+  var filename = 'random.txt';
+  var options =   {
+      root: root,
+      headers: {
+          // "content": "text/html;charset=utf-8",
+          "Content-Type": "application/octet-stream",
+          "Expires":"0",
+          "Cache-Control": "must-revalidate, post-check=0, pre-check=0",
+          "content-disposition": "attachment;filename=" + filename
+      }
+  };
+
+
+
+
+
+  // res.attachment("uploads/textfiles" + fileName);
+  // res.status(200).end();  
+  res.sendFile( filename ,options);
+  // res.status(200).end();
+
+  // res.status(400).send("req.files");
+
+
+// [ { fieldname: 'piousFile',
+//     originalname: 'randomtext_lorem_p.txt',
+//     encoding: '7bit',
+//     mimetype: 'text/plain',
+//     destination: 'public/uploads/textfiles',
+//     filename: '0a803dde5f918979554f9f7d406c69c7',
+//     path: 'public/uploads/textfiles/0a803dde5f918979554f9f7d406c69c7',
+//     size: 978 } ]
+
+
+}]);
 
 module.exports = router;
